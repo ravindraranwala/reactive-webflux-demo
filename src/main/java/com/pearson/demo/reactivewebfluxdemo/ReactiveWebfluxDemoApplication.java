3160a65d18eaeb09c4765c54f0e5f552da4c7839
Ravindra <ravindra.ranwala@pearson.com>
2020-01-31 21:04:54 +0530

    Initial commit of boilerplate project.

---
+++ src/main/java/com/pearson/demo/reactivewebfluxdemo/ReactiveWebfluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pearson.demo.reactivewebfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebfluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebfluxDemoApplication.class, args);
+	}
+
+}
